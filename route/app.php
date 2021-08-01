38f7a803937b285a42beddc5297b5531ba15a8d7
覃霖扬 <9329058029@qq.com>
2021-08-01 14:30:47 +0800

    修改代码

--- route/app.php
+++ route/app.php
@@ -10,8 +10,6 @@
 // +----------------------------------------------------------------------
 use think\facade\Route;
 
-Route::get('think', function () {
-    return 'hello,ThinkPHP6!';
+Route::get('/', function () {
+    return redirect("/admin");
 });
-
-Route::get('hello/:name', 'index/hello');
